ead685dba70f068cd44dffa763cdec138077029f
charescape <charescape@outlook.com>
2021-02-25 14:16:06 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
-function _is_empty_string($value)
-{
-    return is_string($value) && trim($value) === '';
+if (!function_exists('_is_empty_string')) {
+    function _is_empty_string($value)
+    {
+        return is_string($value) && trim($value) === '';
+    }
 }
 
-function _is_full_string($value)
-{
-    return is_string($value) && trim($value) !== '';
+if (!function_exists('_is_full_string')) {
+    function _is_full_string($value)
+    {
+        return is_string($value) && trim($value) !== '';
+    }
 }
+
