334c5ab430c2ebfbd50e0f717c00df283cb843e6
charescape <charescape@outlook.com>
2021-09-16 00:06:16 +0800

    Create IsTest.php

---
+++ tests/IsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Charescape\PhpFunctions\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IsTest extends TestCase {
+    public function test_pf_is_string_filled()
+    {
+        $this->assertTrue(pf_is_string_filled('abc'));
+        $this->assertTrue(pf_is_string_filled('123'));
+
+        $this->assertFalse(pf_is_string_filled(''));
+        $this->assertFalse(pf_is_string_filled(' '));
+        $this->assertFalse(pf_is_string_filled(0));
+        $this->assertFalse(pf_is_string_filled(123));
+        $this->assertFalse(pf_is_string_filled(123.45));
+        $this->assertFalse(pf_is_string_filled(true));
+        $this->assertFalse(pf_is_string_filled(false));
+
+        $this->assertFalse(pf_is_string_filled(null));
+        $this->assertFalse(pf_is_string_filled([]));
+        $this->assertFalse(pf_is_string_filled((object)[]));
+
+
+        $this->assertTrue(pf_is_string_filled('abc', true));
+        $this->assertTrue(pf_is_string_filled('123', true));
+
+        $this->assertFalse(pf_is_string_filled('', true));
+        $this->assertFalse(pf_is_string_filled(' ', true));
+        $this->assertTrue(pf_is_string_filled(0, true));
+        $this->assertTrue(pf_is_string_filled(123, true));
+        $this->assertTrue(pf_is_string_filled(123.45, true));
+        $this->assertFalse(pf_is_string_filled(true, true));
+        $this->assertFalse(pf_is_string_filled(false, true));
+
+        $this->assertFalse(pf_is_string_filled(null, true));
+        $this->assertFalse(pf_is_string_filled([], true));
+        $this->assertFalse(pf_is_string_filled((object)[], true));
+    }
+}
