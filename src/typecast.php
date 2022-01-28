fe245f5e6b883a295fd155451f1e1ac87daadce0
charescape <charescape@outlook.com>
2022-01-28 13:35:49 +0800

    Update typecast.php

--- src/typecast.php
+++ src/typecast.php
@@ -3,18 +3,18 @@
 declare(strict_types=1);
 
 if (!function_exists('pf_str_or_null')) {
-    function pf_str_or_null($value, bool $onlyFilledString = true, bool $useTrim = true): ?string
+    function pf_str_or_null($value, bool $onlyFilledString = true, bool $alterValueUsingTrim = true): ?string
     {
         if (is_numeric($value) && !is_string($value)) {
             $value = (string)$value;
         }
 
         if (is_string($value)) {
-            if ($useTrim) {
+            if ($alterValueUsingTrim) {
                 $value = trim($value);
             }
 
-            if ($onlyFilledString && $value === '') {
+            if ($onlyFilledString && trim($value) === '') {
                 return null;
             }
 
