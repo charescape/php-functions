d772f5ecc3bce7ae7f57edc5c36467646037b46b
charescape <53265646+charescape@users.noreply.github.com>
2024-02-03 21:03:01 +0800

    htmlspecialchars_utf8_mixed

--- src/internal.php
+++ src/internal.php
@@ -7,3 +7,25 @@ if (!function_exists('htmlspecialchars_utf8')) {
         return htmlspecialchars($str, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', $double_encode);
     }
 }
+
+if (!function_exists('htmlspecialchars_utf8_mixed')) {
+    function htmlspecialchars_utf8_mixed($value, bool $double_encode = true): string {
+        if ($value === null) {
+            return 'null';
+        }
+
+        if (is_bool($value)) {
+            return $value ? 'true' : 'false';
+        }
+
+        if (is_int($value) || is_float($value)) {
+            return (string) $value;
+        }
+
+        if (is_string($value)) {
+            return htmlspecialchars_utf8($value, $double_encode);
+        }
+
+        throw new InvalidArgumentException("Invalid argument type [" . gettype($value) . "] for htmlspecialchars_utf8_mixed()");
+    }
+}
--- tests/InternalTest.php
+++ tests/InternalTest.php
@@ -9,11 +9,29 @@ use PHPUnit\Framework\TestCase;
 class InternalTest extends TestCase {
     public function test_htmlspecialchars_utf8()
     {
+        $this->assertSame('abc', htmlspecialchars_utf8('abc'));
         $this->assertSame('&quot;', htmlspecialchars_utf8('"'));
         $this->assertSame('&lt;', htmlspecialchars_utf8('<'));
         $this->assertSame('&gt;', htmlspecialchars_utf8('>'));
         $this->assertSame('&amp;', htmlspecialchars_utf8('&'));
         $this->assertSame('&amp;amp;', htmlspecialchars_utf8('&amp;'));
         $this->assertSame('&amp;', htmlspecialchars_utf8('&amp;', false));
+    }
+
+    public function test_htmlspecialchars_utf8_mixed()
+    {
+        $this->assertSame('abc', htmlspecialchars_utf8_mixed('abc'));
+        $this->assertSame('&quot;', htmlspecialchars_utf8_mixed('"'));
+        $this->assertSame('&lt;', htmlspecialchars_utf8_mixed('<'));
+        $this->assertSame('&gt;', htmlspecialchars_utf8_mixed('>'));
+        $this->assertSame('&amp;', htmlspecialchars_utf8_mixed('&'));
+        $this->assertSame('&amp;amp;', htmlspecialchars_utf8_mixed('&amp;'));
+        $this->assertSame('&amp;', htmlspecialchars_utf8_mixed('&amp;', false));
+
+        $this->assertSame('true', htmlspecialchars_utf8_mixed(true));
+        $this->assertSame('false', htmlspecialchars_utf8_mixed(false));
+        $this->assertSame('null', htmlspecialchars_utf8_mixed(null));
+        $this->assertSame('123', htmlspecialchars_utf8_mixed(123));
+        $this->assertSame('1.23', htmlspecialchars_utf8_mixed(1.23));
     }
 }
