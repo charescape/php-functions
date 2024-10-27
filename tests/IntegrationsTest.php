fdd57fc2cfbe4ea32e8fff57f47490a7fcf0373d
charescape <53265646+charescape@users.noreply.github.com>
2024-10-27 18:02:20 +0800

    Update IntegrationsTest.php

--- tests/IntegrationsTest.php
+++ tests/IntegrationsTest.php
@@ -9,6 +9,9 @@ use PHPUnit\Framework\TestCase;
 class IntegrationsTest extends TestCase {
     public function test_pf_gfm(): void
     {
-        $this->assertSame('<h1>Hello World!</h1>', pf_gfm()->convert('# Hello World!')->getContent());
+        $this->assertSame(
+            '<h1>Hello World!</h1>',
+            trim(pf_gfm()->convert('# Hello World!')->getContent())
+        );
     }
 }
